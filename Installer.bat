b55f6b6b41c8be5a02e52bf50651a5bd2a3e6682
TexDex2 <82395629+TexDex2@users.noreply.github.com>
2021-04-12 09:11:13 -0600

    Add files via upload

---
+++ Installer.bat
@@ -0,0 +1,36 @@
+@echo off
+color a
+cls
+
+echo                  _________-----_____
+echo        ____------           __      ----_
+echo  ___----             ___------              \
+echo     ----________        ----                 \
+echo                -----__    ^|             _____)
+echo                     __-                /     \
+echo         _______-----    ___--          \    /)\
+echo   ------_______      ---____            \__/  /
+echo                -----__    \ --    _          /\
+echo                       --__--__     \_____/   \_/\
+echo                               ---^|   /          ^|
+echo                                  ^| ^|___________^|
+echo                                  ^| ^| ((_(_)^| )_)
+echo                                  ^|  \_((_(_)^|/(_)
+echo                                   \             (
+echo                                    \_____________)
+
+
+                                                                                          
+ECHO =============================
+ECHO INSTALLING
+ECHO =============================
+pip install random
+pip install time
+pip install selenium
+pip install colorama
+pip install tqdm
+pip install beautifulsoup4
+pip install requests
+ECHO =============================
+ECHO COMPLETED
+ECHO =============================
