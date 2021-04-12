9abb24288d53453e259240e07abe9bf884ac16f7
TexDex2 <82395629+TexDex2@users.noreply.github.com>
2021-04-12 09:11:40 -0600

    Add files via upload

---
+++ FireFoxDriver.bat
@@ -0,0 +1,31 @@
+@echo off
+
+color a
+cls
+
+echo                  _________-----_____
+echo        ____------           __      ----_
+echo  ___----             ___------              \
+echo     ----________        ----                 \
+echo                -----__    ^|             _____)
+echo                     __-                /     \
+echo         _______-----    ___--          \    /)\
+echo   ------_______      ---____            \__/  /
+echo                -----__    \ --    _          /\
+echo                       --__--__     \_____/   \_/\
+echo                               ---^|   /          ^|
+echo                                  ^| ^|___________^|
+echo                                  ^| ^| ((_(_)^| )_)
+echo                                  ^|  \_((_(_)^|/(_)
+echo                                   \             (
+echo                                    \_____________)
+
+
+start firefox https://github.com/mozilla/geckodriver/releases
+
+echo Scroll down just a bit until you see geckodriver-v0.29.0
+echo Download the "geckodriver-v0.29.0-win64.zip
+echo After you download that
+echo You must place it in a folder with the program
+echo Press any key . . . .
+pause >nul
